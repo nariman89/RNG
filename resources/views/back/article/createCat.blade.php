6a83087830089fd4bffe397274f8281468d7ca82
nariman89 <nariman.alkurdi@hotmail.com>
2019-06-14 17:45:01 +0200

    admin can add a new category

--- resources/views/back/article/createCat.blade.php
+++ resources/views/back/article/createCat.blade.php
@@ -4,57 +4,21 @@
 		<h1>Create a New Project</h1>
 
 		@include('partials/validation_errors')
-
-		<form method="POST" action="/projects">
-
-			@csrf
-
+ <form method="POST" id="add-form" action="/admin/article">
+ <!-- för the massage efter adding NA-->
+  @if(session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+  @endif
+		{!! Form::open() !!}
 			<div class="form-group">
-				<label for="title">Project Title</label>
-				<input type="text" name="title" id="title" class="form-control" placeholder="Project Title" required value="{{ old('title') }}">
+				<label for="category_name">category name</label>
+				<input type="text" name="category_name" id="name" class="form-control" placeholder="category_name" required value="{{old('category_name')}}">
+                <button type="submit"  class="btn btn-primary">save</button>
+    
+    {!!Form::close()!!}
 			</div>
-
-			<div class="form-group">
-				<label for="description">Project Description</label>
-				<input type="text" name="description" id="description" class="form-control" placeholder="Project Description" required value="{{ old('description') }}">
-			</div>
-
-			<input type="submit" value="Create New Project" class="btn btn-primary">
-		</form>
-		<h1>Create a Category</h1>
-
-{!! Form::open(
-array(
-    'route' => 'categories.store',
-    'class' => 'form')
-) !!}
-
-@if (count($errors) > 0)
-<div class="alert alert-danger">
-    There were some problems adding the category.<br />
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li></li>
-        @endforeach
-    </ul>
-</div>
-@endif
-
-<div class="form-group">
-    {!! Form::label('Category') !!}
-    {!! Form::text('name', null,
-    array(
-        'class'=>'form-control',
-        'placeholder'=>'List Name'
-    )) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::submit('Create Category!',
-    array('class'=>'btn btn-primary'
-    )) !!}
-</div>
-{!! Form::close() !!}
 </div>
 
 		<a href="/projects">&laquo; Back to all Articles</a>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::view('/', 'welcome'); //First page
 Route::middleware(['auth'])->group(function() {
 Route::get('/article/adsArticle', 'ArticleController@create');
 });
+Route::get('/article/index', 'ArticleController@index');
+
 Route::post('/article/adsArticle', 'ArticleController@store');
 Route::get('/category/{id}', 'PageController@adsByCategory');
  Route::get('showDetail/{id}','ArticleController@adsDetails');
@@ -22,4 +24,8 @@ Route::get('/projects/myarticles', 'PageController@show');
 Route::resource('/article', 'ArticleController');
 Route::resource('/bookings/bookfinish', 'BookingController');
 Route::get('admin/article ', 'PageController@admin')->middleware('Isadmin');
+Route::get('/back/article/createCat', 'CategoryController@index');
+Route::post('/admin/article', 'CategoryController@store');
+
+
 
